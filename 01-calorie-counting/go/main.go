37fadc3528a75d21290637ba62998a672e4642a1
Jaime Villarreal <jvillarreal337@gmail.com>
2022-12-01 17:37:14 -0600

    feat: added go solution to 01

---
+++ 01-calorie-counting/go/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// read file
+	// sum calories until blank is found, keep track of highest score
+	max_calories := 0
+	f, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	current_calories := 0
+	for scanner.Scan() {
+		if scanner.Text() != "" {
+			cal, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Fatal(err)
+			}
+			current_calories += cal
+		} else {
+			if max_calories < current_calories {
+				max_calories = current_calories
+			}
+			current_calories = 0
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(max_calories)
+}
