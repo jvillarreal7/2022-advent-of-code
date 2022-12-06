5c72f549983c09df103d2763aaaddb6dd5cbe20f
Jaime Villarreal <jvillarreal337@gmail.com>
2022-12-05 23:35:05 -0600

    feat(06-02): added python solution

---
+++ 06-tuning-trouble/python/part_two.py
@@ -0,0 +1,14 @@
+def get_input_sequence() -> str:
+    with open("../input.txt", "r") as f:
+        return f.readline()
+
+
+def get_marker(sequence: str) -> int:
+    for i in range(14, len(sequence)):
+        sequence_slice = sequence[i - 14 : i]
+        if len(set(sequence_slice)) == 14:
+            return i
+
+
+sequence = get_input_sequence()
+print(get_marker(sequence))
